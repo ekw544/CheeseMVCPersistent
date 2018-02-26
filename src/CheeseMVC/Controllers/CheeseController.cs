88079e5bb7fa6047bf795b295c4fa725e2aeef39
ekw544 <elisawood10@aol.com>
2018-02-25 21:01:46 -0600

    Completed Part 2: Setting up a one-to-many relationship, add Cheeses to CheeseCategory, replace CheeseType with CheeseCategory, updated the ViewModel, Unpdated CheeseController (index, add, add (HttpPost)), updated Views

--- src/CheeseMVC/Controllers/CheeseController.cs
+++ src/CheeseMVC/Controllers/CheeseController.cs
@@ -4,29 +4,32 @@ using System.Collections.Generic;
 using CheeseMVC.ViewModels;
 using CheeseMVC.Data;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace CheeseMVC.Controllers
 {
     public class CheeseController : Controller
     {
-        private CheeseDbContext context;
+        //added the readonly
+        private readonly CheeseDbContext context;
 
         public CheeseController(CheeseDbContext dbContext)
         {
-            context = dbContext;
+            //add the this.
+            this.context = dbContext;
         }
 
         // GET: /<controller>/
         public IActionResult Index()
         {
-            List<Cheese> cheeses = context.Cheeses.ToList();
+            IList<Cheese> cheeses = context.Cheeses.Include(c => c.Category).ToList();
 
             return View(cheeses);
         }
 
         public IActionResult Add()
         {
-            AddCheeseViewModel addCheeseViewModel = new AddCheeseViewModel();
+            AddCheeseViewModel addCheeseViewModel = new AddCheeseViewModel(context.Categories.ToList());
             return View(addCheeseViewModel);
         }
 
@@ -36,11 +39,12 @@ namespace CheeseMVC.Controllers
             if (ModelState.IsValid)
             {
                 // Add the new cheese to my existing cheeses
+                CheeseCategory newCheeseCategory = context.Categories.Single(c => c.ID == addCheeseViewModel.CategoryID);
                 Cheese newCheese = new Cheese
                 {
                     Name = addCheeseViewModel.Name,
                     Description = addCheeseViewModel.Description,
-                    Type = addCheeseViewModel.Type
+                    Category = newCheeseCategory
                 };
 
                 context.Cheeses.Add(newCheese);
@@ -71,6 +75,31 @@ namespace CheeseMVC.Controllers
             context.SaveChanges();
 
             return Redirect("/");
+        }
+
+        //added from video lesson
+        public IActionResult Category(int id)
+        {
+            if (id == 0)
+            {
+                return Redirect("/Category");
+            }
+
+            CheeseCategory theCategory = context.Categories
+                .Include(cat => cat.Cheeses)
+                .Single(cat => cat.ID == id);
+
+            // To query for the cheeses from the other side of the relationship
+            /*
+             IList<Cheese> theCheeses = context.Cheeses
+                .Include(c => c.Category)
+                .Where(c => c.CategoryID == id)
+                .ToList();
+            */
+
+            ViewBag.title = "Cheeses in category: " + theCategory.Name;
+
+            return View("Index", theCategory.Cheeses);
         }
     }
 }
---
+++ src/CheeseMVC/ViewModels/AddCategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.ViewModels
+{
+    public class AddCategoryViewModel
+    {
+        [Required(ErrorMessage = "You must give a name for the category")]
+        //did not explicity say to include error message, but added anyway
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+    }
+}
